0148500362a4c4312bc698b79658bda61ab275fc
Johan Wanglof <jwanglof@gmail.com>
2016-06-29 07:36:56 +0200

    Added support for Browserify

---
+++ index.js
@@ -0,0 +1,7 @@
+// Should already be required, just here for clarity
+require('angular');
+
+// load the angular-ui-router-title module
+require('./angular-ui-router-title');
+
+module.exports = 'ui.router.title';
